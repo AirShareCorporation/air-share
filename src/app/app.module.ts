29af1bd42f44f7a1ffd432e04371c3c683313a4f
DavidB <david.b@hotmail.fr>
2022-05-18 16:03:09 +0200

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ForumComponent } from './forum/forum.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForumComponent,
+    HeaderComponent,
+    FooterComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
