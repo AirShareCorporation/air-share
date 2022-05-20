ea59478ce70aab223e74494b5d64a0c0086ba1bc
KevinLeNozer <kevin.lenozer@gmail.com>
2022-05-20 16:32:36 +0200

    tailwind

--- src/app/services/datas/datas.service.ts
+++ src/app/services/datas/datas.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -31,7 +32,7 @@ export class DatasService {
   // getCity() {
   //   let citys: string[]  = ["paris", "nantes", "poitiers", "marseille"];
   //   for (const element of citys) {
-  //     this.getAirData(element);
+  //    this.getAirData(element);
   //   }
   // }
 }
