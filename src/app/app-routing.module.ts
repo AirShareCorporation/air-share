553255efefaa416530b1ca1946a972a64ac77e51
DavidB <david.b@hotmail.fr>
2022-05-24 10:49:57 +0200

    lazy admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,18 @@ import { MapComponent } from "./map/map.component";
 import { LoginFormComponent } from './forms/login-form/login-form.component';
 import { RegisterFormComponent } from './forms/register-form/register-form.component';
 import { ResetFormComponent } from './forms/reset-form/reset-form.component';
-import {AdminComponent} from "./admin/admin/admin.component";
 import { HomPageComponent } from './hom-page/hom-page.component';
+import { AuthGuard } from './auth/auth.guard';
 
 
 
 const routes: Routes = [
   { path: 'forum', component: ForumComponent },
   { path: 'map', component: MapComponent },
-  { path: 'admin', component: AdminComponent },
+  { path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+    canLoad: [AuthGuard]
+  },
   { path: 'Accueil', component: HomPageComponent },
   { path: 'inscription', component: RegisterFormComponent },
   { path: 'login', component: LoginFormComponent },
